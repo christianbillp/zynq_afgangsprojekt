7f8a162582ab25e208e9e3a0e010138a99653c2a
christianbillp <christianbillp@gmail.com>
2018-12-23 02:05:54 +0100

    Can do some more

--- python/simple_convolution.py
+++ python/simple_convolution.py
@@ -2,8 +2,12 @@
 import torch
 from torch.nn import Conv2d, Module
 from torch.nn.functional import relu, max_pool2d, log_softmax
+import numpy as np
 
-class Net(Module):
+k_dim = 3
+kernel = torch.ones([k_dim, k_dim])
+
+class Net1(Module):
     """Network structure:
         Convolution -> Maxpooling -> log_softmax
         1 kernel, 5x5    2x2
@@ -11,17 +15,43 @@ class Net(Module):
     
     """
     def __init__(self):
-        super(Net, self).__init__()
+        super(Net1, self).__init__()
         self.conv1 = Conv2d(1, 1, kernel_size=3)
 
     def forward(self, x):
 #        x = relu(max_pool2d(self.conv1(x), kernel_size=2))
+
         x = self.conv1(x)
+        print("Net1 conv: {}".format(x.shape))
+        x = max_pool2d(x, kernel_size=2)
+        print("Net1 pool: {}".format(x.shape))
+        x = relu(x)
+
+#        return log_softmax(x)
+        return x
+    
+class Net2(Module):
+    """Network structure:
+        Convolution -> Maxpooling -> log_softmax
+        1 kernel, 5x5    2x2
+    
+    
+    """
+    def __init__(self):
+        super(Net2, self).__init__()
+
+    def forward(self, x):
+        x = sconv(x, 0)
+        print("Net2 conv: {}".format(x.shape))
+        x = spool(x)
+        print("Net2 pool: {}".format(x.shape))
+        x = srelu(x)
         
 #        return log_softmax(x)
         return x
 
-network = Net()
+network1 = Net1()
+network2 = Net2()
 
 # Create picture of ones
 picture = torch.ones([28,28])
@@ -33,43 +63,73 @@ data = picture.unsqueeze(0).unsqueeze(0)
 mnist_example = torch.load('mnist_example.pt')
 
 # Set weights to 1
-network.conv1.weight.data.fill_(1)
+network1.conv1.weight.data.fill_(1)
 
 # Set bias to 0
-#network.conv1.bias.data.fill_(0)
+network1.conv1.bias.data.fill_(0)
 
 # Perform inference for a single data object
 #result = network(data)
-result = network(mnist_example)
-print(result)
+result1 = network1(mnist_example)
+result2 = network2(mnist_example)
+diff = result1-result2
+non_zero = np.count_nonzero(diff.detach().numpy())
+
+print("""Results:
+    Net1: {}
+    Net2: {}
+    Non_zero: {}""".format(result1.shape, result2.shape, non_zero))
 
 #%%
-print(network.conv1.weight.shape)
-print(network.conv1.weight)
+def nsoftmax_log(image):
+    for row in range(image.shape[0]):
+        for column in range(image.shape[1]):
+            if image[row][column] < 0:
+                image[row][column] = 0
+    return image
+    np.log( np.exp(x_i) / np.exp(x).sum() )
+
+
 #%%
-def show(data):
-    for row in data:
-        for value in row:
-            print(round(float(value),2),end=' ')
-        print("")
+def nrelu(image):
+    for row in range(image.shape[0]):
+        for column in range(image.shape[1]):
+            if image[row][column] < 0:
+                image[row][column] = 0
+    return image
 
-
-#%% Low-level convolution without bias
-
-kernel = torch.tensor([[1.0, 0, 0],
-                       [0, 1.0, 0],
-                       [0, 0 ,1.0]])
-
-kernel = torch.tensor([[2, -10, 0],
-                       [2, -10, 0],
-                       [2, -10, 0]], dtype=torch.float32)
-
-kernel = torch.ones([3,3])
-kernel_size = int((kernel.shape[0] - 1) / 2)
-
-def convolute(image, kernel):
-    # Prototype for C function
+def srelu(image):
+    output = nrelu(image[0][0]).unsqueeze(0).unsqueeze(0)
+    return output
+                
     
+    
+#%% Low-level convolution without bias
+kernels = {'sharpen': torch.tensor([[ 0,-1, 0],
+                                    [-1, 5,-1],
+                                    [ 0,-1, 0]], dtype=torch.float32),
+            'edge_detect' :  torch.tensor([[-1,-1,-1],
+                                           [-1, 8,-1],
+                                           [-1,-1,-1]], dtype=torch.float32),
+            'emboss' :  torch.tensor([[-2,-1, 0],
+                                      [-1, 1, 1],
+                                      [ 0, 1, 2]], dtype=torch.float32),
+}
+
+#kernel = torch.tensor([[1.0, 0, 0],
+#                       [0, 1.0, 0],
+#                       [0, 0 ,1.0]])
+    
+#k_dim = 7
+#kernel = torch.ones([k_dim, k_dim])
+
+
+def convolute(image, kernel, bias):
+    # Performs a single convolution
+    
+    
+    kernel_size = int((kernel.shape[0] - 1) / 2)
+
     # Create smaller output tensor
     output = torch.zeros([image.shape[0] - kernel_size*2, image.shape[1] - kernel_size*2])
     
@@ -79,18 +139,43 @@ def convolute(image, kernel):
             for kernel_row in range(kernel.shape[0]):
                 for kernel_column in range(kernel.shape[1]):
                     total += image[row + kernel_row][column + kernel_column]* kernel[kernel_row, kernel_column]
-    
-            output[row][column] = total
+
+            # Activation function goes here
+            output[row][column] = total+bias
             
     return output
 
+def sconv(image, bias):
+    output = convolute(image[0][0], kernel, bias).unsqueeze(0).unsqueeze(0)
 
-#output = convolute(picture, kernel)
-output = convolute(mnist_example[0][0], kernel)
-#print(output)
-#print(output.shape)
-#show(output)
+    return output
 
+def maxpool(image, kernel_size=2):
+    
+    # Create smaller output tensor
+    output = torch.zeros([image.shape[0]//2, image.shape[1]//2])
+    
+    for row in range(image.shape[0]//2):
+        for column in range(image.shape[1]//2):
+ 
+            # Check for largest value
+            temp = torch.zeros([kernel_size*2])
+            temp[0] = image[row*2][column*2]
+            temp[1] = image[row*2+1][column*2]
+            temp[2] = image[row*2][column*2+1]
+            temp[3] = image[row*2+1][column*2+1]
+            output[row][column] = max(temp)
+    
+    return output
+
+def spool(image):
+    output = maxpool(image[0][0]).unsqueeze(0).unsqueeze(0)
+    return output
+
+
+
+            
+#%%
 
 
 import matplotlib.pyplot as plt
@@ -104,10 +189,9 @@ def compare(a, b):
     plt.title('convoluted')
 
 
-compare(mnist_example[0][0], output)
+#compare(mnist_example[0][0], output)
+compare(result1.detach().numpy()[0][0],  result2.detach().numpy())
 
-
-#%%
 def check_data(data):
     print("Diagonal:")
     for row in range(data.shape[0]):
@@ -124,3 +208,9 @@ def check_output(data):
 
 check_data(mnist_example[0][0])
 check_output(output)
+
+def show(data):
+    for row in data:
+        for value in row:
+            print(round(float(value),2),end=' ')
+        print("")
